6d1a0e1f7b534250e2198208b7620571f185c63f
Chris Doherty <chris.doherty4@gmail.com>
2023-01-04 22:20:41 -0600

    Remove deprecated backends
    Signed-off-by: Chris Doherty <chris.doherty4@gmail.com>

--- job/http.go
+++ job/http.go
@@ -1,8 +1,6 @@
 package job
 
 import (
-	"bytes"
-	"encoding/json"
 	"io"
 	"net/http"
 	"path"
@@ -22,72 +20,11 @@ func (j Job) ServeFile(w http.ResponseWriter, req *http.Request, i Installers) {
 }
 
 func (j Job) ServePhoneHomeEndpoint(w http.ResponseWriter, req *http.Request) {
-	var b []byte
-
-	switch req.Header.Get("Content-Type") {
-	case "application/json", "":
-		// cloudbase-init sends json without any content-type header
-		// so we include "" in our case and parse the body as json
-		var err error
-		b, err = readClose(req.Body)
-		if err != nil {
-			j.Error(errors.WithMessage(err, "reading phone-home body"))
-			w.WriteHeader(http.StatusBadRequest)
-
-			return
-		}
-	case "application/x-www-form-urlencoded":
-		// We convert urlencoded values to JSON
-		if err := req.ParseForm(); err != nil {
-			j.Error(errors.Wrap(err, "parsing http form"))
-			w.WriteHeader(http.StatusBadRequest)
-
-			return
-		}
-
-		postData := map[string]string{}
-		for key, value := range req.PostForm {
-			postData[key] = value[0]
-		}
-
-		buf := new(bytes.Buffer)
-		_ = json.NewEncoder(buf).Encode(postData)
-		b = buf.Bytes()
-	default:
-		// Any other content types equal a bad request
-		w.WriteHeader(http.StatusBadRequest)
-
-		return
-	}
-
-	j.phoneHome(req.Context(), b)
-
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte{})
 }
 
 func (j Job) ServeProblemEndpoint(w http.ResponseWriter, req *http.Request) {
-	b, err := readClose(req.Body)
-	if err != nil {
-		j.Error(errors.WithMessage(err, "reading problem body"))
-		w.WriteHeader(http.StatusBadRequest)
-
-		return
-	}
-	var v struct {
-		Problem string `json:"problem"`
-	}
-	if err := json.Unmarshal(b, &v); err != nil {
-		j.Error(errors.Wrap(err, "parsing problem body as json"))
-		w.WriteHeader(http.StatusBadRequest)
-
-		return
-	}
-	if !j.PostHardwareProblem(req.Context(), v.Problem) {
-		w.WriteHeader(http.StatusBadGateway)
-
-		return
-	}
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte{})
 }
